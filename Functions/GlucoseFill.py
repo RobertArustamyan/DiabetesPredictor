286f0836fa583dc2cf887dfb5a39fbed32d985d6
RobertArustamyan <robertarustamyan2@gmail.com>
2024-08-02 12:44:21 +0400

    Modified GlucoseFill.py

--- Functions/GlucoseFill.py
+++ Functions/GlucoseFill.py
@@ -4,13 +4,13 @@ import numpy as np
 class GlucoseFiller:
     def __init__(self, df):
         self._df = df
-        self._median_glucose_outcome_1 = df[df['Outcome'] == 1]['Glucose'].median()
-        self._mode_glucose_outcome_0 = df[df['Outcome'] == 0]['Glucose'].mode()[0]
+        self._median_glucose_outcome_1 = df[(df['Outcome'] == 1) & (df['Glucose'] != 0)]['Glucose'].median()
+        self._mode_glucose_outcome_0 = df[(df['Outcome'] == 0) & (df['Glucose'] != 0)]['Glucose'].mode()[0]
         self._fill_glucose()
 
     def _fill_glucose(self):
         for index,row in self._df.iterrows():
-            if pd.isna(row['Glucose'] or row['Glucose'] == 0):
+            if pd.isna(row['Glucose']) or row['Glucose'] == 0:
                 if row['Outcome'] == 1:
                     self._df.at[index,'Glucose'] = self._median_glucose_outcome_1
                 else:
