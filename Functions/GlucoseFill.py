68fb58d17de518e6c98c85aa531b37305c08dc4b
RobertArustamyan <robertarustamyan2@gmail.com>
2024-08-02 11:25:47 +0400

    Modified GlucoseFill.py

--- Functions/GlucoseFill.py
+++ Functions/GlucoseFill.py
@@ -1,27 +1,21 @@
 import pandas as pd
 import numpy as np
-def fill_glucose(df: pd.DataFrame):
-    # Calculate median and mode for the respective Outcome groups
-    median_glucose_outcome_1 = df[df['Outcome'] == 1]['Glucose'].median()
-    mode_glucose_outcome_0 = df[df['Outcome'] == 0]['Glucose'].mode()[0]
-    # Apply the filling function with additional arguments
-    df['Glucose'] = df.apply(
-        fill,
-        axis=1,
-        median_glucose_outcome_1=median_glucose_outcome_1,
-        mode_glucose_outcome_0=mode_glucose_outcome_0
-    )
 
-def fill(row, median_glucose_outcome_1, mode_glucose_outcome_0):
-    if pd.isna(row['Glucose']):
-        if row['Outcome'] == 1:
-            return median_glucose_outcome_1
-        elif row['Outcome'] == 0:
-            return mode_glucose_outcome_0
-    else:
-        return row['Glucose']
+class GlucoseFiller:
+    def __init__(self, df):
+        self._df = df
+        self._median_glucose_outcome_1 = df[df['Outcome'] == 1]['Glucose'].median()
+        self._mode_glucose_outcome_0 = df[df['Outcome'] == 0]['Glucose'].mode()[0]
+        self._fill_glucose()
 
-if __name__ == '__main__':
-    df = pd.read_csv('../Data/diabetes.csv')
-    df.loc[df['Glucose'] == 0, 'Glucose'] = np.nan
-    fill_glucose(df)
+    def _fill_glucose(self):
+        for index,row in self._df.iterrows():
+            if pd.isna(row['Glucose'] or row['Glucose'] == 0):
+                if row['Outcome'] == 1:
+                    self._df.at[index,'Glucose'] = self._median_glucose_outcome_1
+                else:
+                    self._df.at[index,'Glucose'] = self._mode_glucose_outcome_0
+    @property
+    def get_dataframe(self):
+        return self._df
+
